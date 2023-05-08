ecb62209221d9bbcbf7adcdf68846833c3a2497f
xaavii0711 <xaavii0711@gmail.com>
2023-05-08 16:58:01 +0200

    third commit

--- railway_django_mysql/settings.py
+++ railway_django_mysql/settings.py
@@ -73,10 +73,15 @@ WSGI_APPLICATION = 'railway_django_mysql.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'railway',
+        'USER' : 'root',
+        'PASSWORD' : 'Superlocal123',
+        'HOST' : 'containers-us-west-98.railway.app',
+        'PORT' : '7346',
     }
 }
 
